62f5cb2f6b1dc1329dd5437ceeaa7b27077773e7
JD17VM <jdiegodiego4@gmail.com>
2025-05-25 03:57:08 -0500

    :sparkles: feat(cpp): implement insertion sort in algorithms.h

---
+++ asymptotic-algorithm-analysis/c++/algorithms.h
@@ -0,0 +1,16 @@
+#include <vector>
+#include <cstddef>
+
+using namespace std;
+
+void insertion_sort(vector<int>& arr) {
+    for (int i = 1; i < arr.size(); ++i) {
+        int key = arr[i];
+        int j = i - 1;
+        while (j >= 0 && key < arr[j]) {
+            arr[j + 1] = arr[j];
+            j -= 1;
+        }
+        arr[j + 1] = key;
+    }
+}
