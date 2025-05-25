8f390c5163c3e542b1faff38cd02ec3671385c98
JD17VM <jdiegodiego4@gmail.com>
2025-05-25 03:28:52 -0500

    :sparkles: feat(cpp): add generate_random_data function for test data creation

--- asymptotic-algorithm-analysis/c++/main.cpp
+++ asymptotic-algorithm-analysis/c++/main.cpp
@@ -1,8 +1,20 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <cstdlib>
 
 using namespace std;
+
+vector<int> generate_random_data(int size) {
+    srand(time(0));
+
+    vector<int> data;
+    data.reserve(size);
+    for (int i = 0; i < size; ++i) {
+        data.push_back(rand() % (size * 10 + 1));
+    }
+    return data;
+}
 
 vector<int> generate_worst_case_data(int size) {
     vector<int> data;
@@ -15,8 +27,8 @@ vector<int> generate_worst_case_data(int size) {
 
 int main() {
     int size = 200;
-
-    vector<int> data = generate_worst_case_data(size);
+    //vector<int> data = generate_worst_case_data(size);
+    vector<int> data = generate_random_data(size);
 
     for (int number : data) {
         cout << number << " ";
